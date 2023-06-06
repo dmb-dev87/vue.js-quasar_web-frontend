0922714359fea003544b86780e96220c45e216b1
dmb-dev87 <dmbdev800@gmail.com>
2023-06-06 05:30:34 -0400

    [dmb-dev87][2023-06-06][build vue.js & quasar framework project]

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated>
+      <q-toolbar class="bg-amber-10">
+        <q-toolbar-title>
+          Login
+        </q-toolbar-title>
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="leftDrawer = !leftDrawer"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <!-- (Optional) A Drawer; you can add one more with side="right" or change this one's side -->
+    <q-drawer
+      v-model="leftDrawer"
+      side="left"
+      bordered
+      content-class="bg-grey-2"
+    >
+      <!-- QScrollArea is optional -->
+      <q-scroll-area class="fit q-pa-sm">
+        <div class="row justify-center q-my-lg">
+          <p class="text-h2 text-weight-bolder">
+            LOGO
+          </p>
+        </div>
+        <div class="row q-pl-md q-my-lg">
+          <p class="text-h5 text-weight-mdeium text-amber-10">
+            Services
+          </p>
+        </div>
+        <div class="row q-pl-md q-my-lg">
+          <p class="text-weight-mdeium" style="font-size:18px;">
+            <q-icon name="logout" color="amber-10"/>
+            <span class="q-ml-lg">Logout</span>
+          </p>
+        </div>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+      <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+        <q-btn fab icon="keyboard_arrow_up" color="accent" />
+      </q-page-scroller>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  // name: 'LayoutName',
+
+  setup() {
+    const leftDrawer = ref(false)
+    return { leftDrawer }
+  }
+})
+</script>
---
+++ src/pages/Auth/Login.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <q-page
+    class="window-height window-width row justify-center items-center"
+  >
+    <div class="column q-pa-lg">
+      <div class="row">
+        <q-card square class="shadow-24">
+          <q-card-section>
+            <q-form class="q-px-sm q-pt-xl">
+              <q-input class="q-my-sm" square clearable v-model="url" label="Url" hint="Enter the url of your installation">
+                <template v-slot:prepend>
+                  <q-icon name="link" />
+                </template>
+              </q-input>
+              <q-input class="q-my-sm" square clearable v-model="username" label="Username" hint="Enter your username">
+                <template v-slot:prepend>
+                  <q-icon name="person" />
+                </template>
+              </q-input>
+              <q-input class="q-my-sm" square clearable v-model="password" type="password" label="Password" hint="Enter your password">
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-lg q-my-sm justify-center">
+            <q-btn unelevated size="lg" color="amber-10" class="text-white full-width" label="LOGIN" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Login',
+  data () {
+    return {
+      url: '',
+      username: '',
+      password: ''
+    }
+  }
+})
+</script>
