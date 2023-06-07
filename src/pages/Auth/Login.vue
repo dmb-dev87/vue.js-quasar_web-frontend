e3b8f92d889ec6d404c15f31249c5fc9baf5331c
dmb-dev87 <dmbdev800@gmail.com>
2023-06-07 03:07:47 -0400

    [dmb-dev87][2023-06-07][add validation in login form]

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -8,29 +8,54 @@
           <div class="col">
             <q-card square class="shadow-24">
               <q-card-section>
-                <q-form class="q-px-lg q-pt-xl">
-                  <q-input class="q-mb-lg" square clearable v-model="url" label="Url" hint="Enter the url of your installation">
+                <q-form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset" class="q-px-lg q-pt-xl">
+                  <q-input
+                    class="q-mb-lg"
+                    ref="urlRef"
+                    v-model="urlVal"
+                    label="URL *"
+                    hint="Enter the url of your installation"
+                    lazy-rules
+                    :rules="urlRules"
+                  >
                     <template v-slot:prepend>
                       <q-icon name="link" />
                     </template>
                   </q-input>
-                  <q-input class="q-mb-lg" square clearable v-model="username" label="Username" hint="Enter your username">
+                  <q-input
+                    class="q-mb-lg"
+                    ref="nameRef"
+                    v-model="nameVal"
+                    label="User Name *"
+                    hint="Enter your username"
+                    lazy-rules
+                    :rules="nameRules"
+                  >
                     <template v-slot:prepend>
                       <q-icon name="person" />
                     </template>
                   </q-input>
-                  <q-input class="q-mb-lg" square clearable v-model="password" type="password" label="Password" hint="Enter your password">
+                  <q-input
+                    class="q-mb-lg"
+                    ref="pwdRef"
+                    v-model="pwdVal"
+                    type="password"
+                    label="Password"
+                    hint="Enter your password"
+                    lazy-rules
+                    :rules="pwdRules"
+                  >
                     <template v-slot:prepend>
                       <q-icon name="lock" />
                     </template>
                   </q-input>
+                  <div class="row q-py-lg justify-center">
+                    <div class="col-6 col-md-4">
+                      <q-btn size="lg" color="amber-10" class="text-white full-width" label="LOGIN" type="submit"/>
+                    </div>
+                  </div>
                 </q-form>
               </q-card-section>
-              <q-card-actions class="q-py-xl q-my-lg justify-center">
-                <div class="col-4">
-                  <q-btn unelevated size="lg" color="amber-10" class="text-white full-width" label="LOGIN" @click="updateState" />
-                </div>
-              </q-card-actions>
             </q-card>
           </div>
         </div>
@@ -42,20 +67,73 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useStore } from 'src/store'
+import { ref } from 'vue'
 
 export default defineComponent({
   name: 'Login',
-  data () {
-    return {
-      url: '',
-      username: '',
-      password: ''
-    }
-  },
   setup() {
     const store = useStore()
 
-    return { store }
+    const urlVal = ref(null)
+    const urlRef = ref(null)
+
+    const nameVal = ref(null)
+    const nameRef = ref(null)
+
+    const pwdVal = ref(null)
+    const pwdRef = ref(null)
+
+    return {
+      store,
+
+      urlVal,
+      urlRef,
+      urlRules: [
+        (val: string) => (val && val.length > 0) || 'URL is required',
+        (val: string) => (val && val.search("http") == -1) || 'Please double check the fields',
+      ],
+
+      nameVal,
+      nameRef,
+      nameRules: [
+        (val: string) => (val && val.length > 0) || 'Name is required'
+      ],
+
+      pwdVal,
+      pwdRef,
+      pwdRules: [
+        (val: string) => (val && val.length > 0) || 'Password is required'
+      ],
+
+      onSubmit () {
+        const valid = () => {
+          urlRef.value.validate()
+          nameRef.value.validate()
+          pwdRef.value.validate()
+
+          if (urlRef.value.hasError || nameRef.value.hasError || pwdRef.value.hasError) {
+            return false
+          } else {
+            return true
+          }
+        }
+
+        if (valid) {
+          store.commit('setUrl', urlVal)
+          store.commit('setUsername', nameVal)
+        }
+      },
+
+      onReset () {
+        urlVal.value = null
+        nameVal.value = null
+        pwdVal.value = null
+
+        urlRef.value.resetValidation()
+        nameRef.value.resetValidation()
+        pwdRef.value.resetValidation()
+      }
+    }
   },
   methods: {
     myTweak (offset: number) {
@@ -65,12 +143,6 @@ export default defineComponent({
       } else {
         return { minHeight: `0px` }
       }
-
-    },
-    updateState () {
-      console.log("+++++++++++++++++", this.store)
-      this.store.commit('setUrl', this.url)
-      this.store.commit('setUsername', this.username)
     }
   }
 })
