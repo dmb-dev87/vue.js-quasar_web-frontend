bfee28e8b34c8e4475d2a4f70b486f9e4456d0f7
dmb-dev87 <dmbdev800@gmail.com>
2023-06-19 10:33:00 -0400

    [dmb-dev87][2023-06-19][changed the tracking gps feature]

--- src/services/PositionService.ts
+++ src/services/PositionService.ts
@@ -18,29 +18,56 @@ const checkLocations = (date: any, time: any) => {
 }
 
 const savePostion = (trackgps: boolean) => {
+  const url = myStore.state.authentication.url
+  const userId = myStore.state.authentication.userId
+  const token = myStore.state.authentication.token
+
+  let result = false
+
+  const saveCurrentLocation = (position: any): boolean => {
+    const api = axios.create({
+      baseURL: getBaseUrl(url),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    api.post(
+      `/api/save-location/?userId=${userId}&token=${token}`,
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        speed: position.coords.speed,
+        altitude: position.coords.altitude
+      }
+    ).then((res: any) => {
+      return res.data.status
+    }).catch(() => {
+      return false
+    })
+
+    return false
+  }
+
   if (trackgps) {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
+          result = saveCurrentLocation(position)
           myStore.commit('authentication/setPosition', {
-            trackgps: trackgps,
+            trackgps: true,
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
             speed: position.coords.speed,
-            altitude: position.coords.altitude
+            altitude: position.coords.altitude,
           })
         },
-        (error: any) => {
-          myStore.commit('authentication/setPosition', {
-            trackgps: false,
-            latitude: "0",
-            longitude: "0",
-            speed: null,
-            altitude: null
-          })
+        (error) => {
+          result = false
         }
       )
     } else {
+      result = false
       myStore.commit('authentication/setPosition', {
         trackgps: false,
         latitude: "0",
@@ -50,6 +77,17 @@ const savePostion = (trackgps: boolean) => {
       })
     }
   } else {
+    result = false
+    myStore.commit('authentication/setPosition', {
+      trackgps: false,
+      latitude: "0",
+      longitude: "0",
+      speed: null,
+      altitude: null
+    })
+  }
+
+  if (result == false) {
     myStore.commit('authentication/setPosition', {
       trackgps: false,
       latitude: "0",
